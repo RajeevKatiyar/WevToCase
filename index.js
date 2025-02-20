4ec6ed6d6bc453c2081b996a61f8aa3b8f14f17f
RajeevKatiyar <rajeev@cloudsciencelabs.com>
2025-02-20 15:50:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -74,76 +74,147 @@
 
 
 
-        const tokenUrl = "https://saas-inspiration-2755--dev.sandbox.my.salesforce.com/services/oauth2/token";
-        const clientId = "3MVG9HbBKF.OyqUfhGc_mmKgDGcg2wr._iLh.sFF8SsXfNbsf8lgPfFqjlHpQNKY03etaQWJJAuG_V4Fo1JCt";
-        const clientSecret = "67451A422D75C3663A054AB7E20686C325E3AD82490336D742DB6B0EA8D7E82C";
-        const username = "aksharma@cloudsciencelabs.com.mcp.dev";
-        const password = "Logmein@143";
-        const securityToken = "rz6e9PAERcjn111DAiEpcDlg";
+        // const tokenUrl = "https://saas-inspiration-2755--dev.sandbox.my.salesforce.com/services/oauth2/token";
+        // const clientId = "3MVG9HbBKF.OyqUfhGc_mmKgDGcg2wr._iLh.sFF8SsXfNbsf8lgPfFqjlHpQNKY03etaQWJJAuG_V4Fo1JCt";
+        // const clientSecret = "67451A422D75C3663A054AB7E20686C325E3AD82490336D742DB6B0EA8D7E82C";
+        // const username = "aksharma@cloudsciencelabs.com.mcp.dev";
+        // const password = "Logmein@143";
+        // const securityToken = "rz6e9PAERcjn111DAiEpcDlg";
 
-        function getAccessToken() {
-            console.log("Fetching Salesforce OAuth token...");
-            const data = {
-                grant_type: 'password',
-                client_id: clientId,
-                client_secret: clientSecret,
-                username: username,
-                password: password + securityToken
-            };
+        // function getAccessToken() {
+        //     console.log("Fetching Salesforce OAuth token...");
+        //     const data = {
+        //         grant_type: 'password',
+        //         client_id: clientId,
+        //         client_secret: clientSecret,
+        //         username: username,
+        //         password: password + securityToken
+        //     };
 
-            return fetch(tokenUrl, {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-                body: new URLSearchParams(data)
-            })
-                .then(response => response.json())
-                .then(data => {
-                    console.log("OAuth Token Received:", data);
-                    return data.access_token;
-                })
-                .catch(error => console.error("Error fetching access token:", error));
-        }
+        //     return fetch(tokenUrl, {
+        //         method: 'POST',
+        //         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        //         body: new URLSearchParams(data)
+        //     })
+        //         .then(response => response.json())
+        //         .then(data => {
+        //             console.log("OAuth Token Received:", data);
+        //             return data.access_token;
+        //         })
+        //         .catch(error => console.error("Error fetching access token:", error));
+        // }
 
-        function uploadFileToSalesforce(file, accessToken) {
-            const fileReader = new FileReader();
-            fileReader.onload = function (event) {
-                const fileData = event.target.result.split(',')[1];
+        // function uploadFileToSalesforce(file, accessToken) {
+        //     const fileReader = new FileReader();
+        //     fileReader.onload = function (event) {
+        //         const fileData = event.target.result.split(',')[1];
 
-                const uploadUrl = 'https://saas-inspiration-2755--dev.sandbox.my.salesforce.com/services/data/v54.0/sobjects/ContentVersion/';
-                const requestData = {
-                    Title: file.name,
-                    PathOnClient: file.name,
-                    VersionData: fileData
-                };
+        //         const uploadUrl = 'https://saas-inspiration-2755--dev.sandbox.my.salesforce.com/services/data/v54.0/sobjects/ContentVersion/';
+        //         const requestData = {
+        //             Title: file.name,
+        //             PathOnClient: file.name,
+        //             VersionData: fileData
+        //         };
 
-                fetch(uploadUrl, {
-                    method: 'POST',
-                    headers: {
-                        'Authorization': 'Bearer ' + accessToken,
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(requestData)
-                })
-                    .then(response => response.json())
-                    .then(data => {
-                        console.log("File uploaded successfully:", data);
-                         const contentDocumentId = data.id; 
-                        console.log('contentDocumentId--------------------------',contentDocumentId);
-                        document.getElementById('00Nbf000001GLr7').value = contentDocumentId;
+        //         fetch(uploadUrl, {
+        //             method: 'POST',
+        //             headers: {
+        //                 'Authorization': 'Bearer ' + accessToken,
+        //                 'Content-Type': 'application/json'
+        //             },
+        //             body: JSON.stringify(requestData)
+        //         })
+        //             .then(response => response.json())
+        //             .then(data => {
+        //                 console.log("File uploaded successfully:", data);
+        //                  const contentDocumentId = data.id; 
+        //                 console.log('contentDocumentId--------------------------',contentDocumentId);
+        //                 document.getElementById('00Nbf000001GLr7').value = contentDocumentId;
                         
-                    })
-                    .catch(error => console.error("Error uploading file:", error));
-            };
-            fileReader.readAsDataURL(file);
-        }
+        //             })
+        //             .catch(error => console.error("Error uploading file:", error));
+        //     };
+        //     fileReader.readAsDataURL(file);
+        // }
 
-        document.getElementById('fileInput').addEventListener('change', function (event) {
-            const file = event.target.files[0];
-            if (file) {
-                console.log("File selected:", file.name);
-                getAccessToken().then(accessToken => {
-                    uploadFileToSalesforce(file, accessToken);
-                });
-            }
+        // document.getElementById('fileInput').addEventListener('change', function (event) {
+        //     const file = event.target.files[0];
+        //     if (file) {
+        //         console.log("File selected:", file.name);
+        //         getAccessToken().then(accessToken => {
+        //             uploadFileToSalesforce(file, accessToken);
+        //         });
+        //     }
+        // });
+
+
+
+
+const tokenUrl = "https://zedonk--zedonkdev.sandbox.my.salesforce.com/services/oauth2/token";
+const clientId = "3MVG9kfeuo6xCm.qoV.HA53lfC9QhRr5cKmiCjODjswL5DoydxiCyUA4Pdt625kYbnOEwHFWT4MWwM8YxEBwU";
+const clientSecret = "32B0FFE870DC26B6C41A34C4E946F4180643A5158B78E48ABD39E11ABC93C039";
+
+function getAccessToken() {
+    console.log("Fetching Salesforce OAuth token...");
+    const data = {
+        grant_type: 'client_credentials',
+        client_id: clientId,
+        client_secret: clientSecret,
+    };
+
+    return fetch(tokenUrl, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: new URLSearchParams(data)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log("OAuth Token Received:", data);
+            return data.access_token;
+        })
+        .catch(error => console.error("Error fetching access token:", error));
+}
+
+function uploadFileToSalesforce(file, accessToken) {
+    const fileReader = new FileReader();
+    fileReader.onload = function (event) {
+        const fileData = event.target.result.split(',')[1];
+
+        const uploadUrl = 'https://zedonk--zedonkdev.sandbox.lightning.force.com/services/data/v54.0/sobjects/ContentVersion/';
+        const requestData = {
+            Title: file.name,
+            PathOnClient: file.name,
+            VersionData: fileData
+        };
+
+        fetch(uploadUrl, {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer ' + accessToken,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestData)
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log("File uploaded successfully:", data);
+                 const contentDocumentId = data.id; 
+                console.log('contentDocumentId--------------------------',contentDocumentId);
+                document.getElementById('00NUA000001N4dX').value = contentDocumentId;
+                
+            })
+            .catch(error => console.error("Error uploading file:", error));
+    };
+    fileReader.readAsDataURL(file);
+}
+
+document.getElementById('fileInput').addEventListener('change', function (event) {
+    const file = event.target.files[0];
+    if (file) {
+        console.log("File selected:", file.name);
+        getAccessToken().then(accessToken => {
+            uploadFileToSalesforce(file, accessToken);
         });
+    }
+});
     
